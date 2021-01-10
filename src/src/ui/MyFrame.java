b467517b5e556bc08df5b8e2c5c5ca99ff26bdb7
svolterra <sarahkalmm1902@gmail.com>
2021-01-09 22:18:08 -0800

    - Added "Click me!" button functionality.

--- src/src/ui/MyFrame.java
+++ src/src/ui/MyFrame.java
@@ -1,29 +1,63 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class MyFrame extends JFrame {
+    private MyPanel panel;
+    private JLabel motivationLabel;
+    private List<String> motivations = new ArrayList<>();
 
-    MyPanel panel;
-
-    MyFrame(){
-
-
+    public MyFrame() {
         panel = new MyPanel();
+        motivationLabel = new JLabel("", SwingConstants.CENTER);
+        motivationLabel.setBounds(110, 10, 600, 200);
+        add(motivationLabel);
 
-        JButton encourageButton = new JButton();
-//        encourageButton.setForeground(new Color(228, 181, 236, 255));
-//        encourageButton.setBackground(new Color(187, 249, 255, 245));
-        encourageButton.setBounds(220,250,100,80);
-//        encourageButton.addActionListener(this);
 
-        this.add(encourageButton);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JButton encourageButton = new JButton("Click me!");
+        encourageButton.setBounds(220, 260, 200, 80);
+        encourageButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                List<String> encouragements = readTextFile();
+                Random rand = new Random();
+                String randomString = encouragements.get(rand.nextInt(encouragements.size()));
+                motivationLabel.setText(randomString);
+                motivationLabel.setFont(new Font("SansSerif", Font.ITALIC, 15));
+                motivationLabel.setForeground(new Color(0,0,0));
+            }
+        });
 
-        this.add(panel);
-        this.pack();
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
+        add(encourageButton);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(panel);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
 
+    }
+
+    public List<String> readTextFile() {
+        try {
+            File encouragementTxt = new File("encouraging-sentences.txt");
+            Scanner myReader = new Scanner(encouragementTxt);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                motivations.add(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error has occurred.");
+        }
+        return motivations;
     }
 }
--- src/src/ui/MyPanel.java
+++ src/src/ui/MyPanel.java
@@ -2,19 +2,18 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.Image;
 
-public class MyPanel extends JPanel  {
+public class MyPanel extends JPanel {
+    private Image background;
 
-
-    Image background;
-    MyPanel(){
+    public MyPanel() {
         background = new ImageIcon("sheep.png").getImage();
-        this.setPreferredSize(new Dimension(640,381));
-    }
-    public void paint(Graphics g){
-        Graphics2D g2D = (Graphics2D) g;
+        this.setPreferredSize(new Dimension(640, 381));
 
+    }
+
+    public void paint(Graphics g) {
+        Graphics2D g2D = (Graphics2D) g;
         g2D.drawImage(background, 0, 0, null);
     }
 
